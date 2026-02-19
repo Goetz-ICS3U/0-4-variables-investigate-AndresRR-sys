23faeb9c479d6803b9ace3789c49c9d4e648b76b
AndresRR-sys <353289382@educ.dpcdsb.org>
2026-02-19 12:33:46 -0500

    Completed Variables 0.4

---
+++ var_investigate.py
@@ -0,0 +1,32 @@
+"""
+author:
+date:
+Investigating Variables
+"""
+
+# Input
+name = "Andres"
+age = 16
+grade = 11
+school = "father michael goetz"
+favourite_exclamation = "AHHH"
+least_favourite_colour = "yellow"
+is_cool = True
+math_test_score = 90.15
+sister_name = "Laylah"
+brother_name = "adrian"
+slope = 2
+y_intercept = 10
+x = 3
+y = slope * x + y_intercept
+
+# Processing / Output
+print(
+    f"Hey guys, my name is {name} and I am {age} years old. I am currently in grade {grade} and go to {school}. "
+    + f"Yesterday, there was a spider in the classroom that made me yell {favourite_exclamation} while I was taking a math test with my {least_favourite_colour} pen :/ "
+    + f"One of the questions on the test was wether or not I thought Elvis was cool, so I obviously put {is_cool} to get the answer right... "
+    + f"Turns out that I only scored a {math_test_score} on the test. One of the qusetions was asking me to write the equation for a line "
+    + f"with a slope of {slope} and a y-intercept of {y_intercept} so I wrote y = {slope}x + {y_intercept}, but I did not notice the x value of {x} in the question "
+    + f"so I needed to, on the next line, say that the value of y was {y}. "
+    + f"I told my sister {sister_name} and my brother {brother_name} about this and they just laughed at me 🙄"
+    + "Anyway, that's the story of my math test." )
